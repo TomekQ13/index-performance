7d8bb156e2eae3b2433a052e4aa50735400532fd
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 17:07:36 +0100

    change run command

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec index-performance_postgres_1 bash && echo "test" && su postgres && bash /home/postgres/runTests.sh
+docker-compose up -d && docker exec index-performance_postgres_1 bash /home/postgres/runTests.sh
