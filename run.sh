e34ad55713a5c00de19c61b9fc6ca11869740bad
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 16:04:04 +0100

    added cat

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<EOF
+docker-compose up -d && cat < docker exec -t index-performance_postgres_1 bash <<EOF
     su postgres
     psql < /home/postgres/inital-tests.sql
 EOF \
