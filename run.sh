3f32d34f6e66d16e23c2a48e9cbee53d9a248e21
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 14:54:41 +0100

    removed tty

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec -it index-performance_postgres_1 bash <<END
+docker-compose up -d && docker exec -T index-performance_postgres_1 bash <<END
     su postgres
     psql < /home/postgres/inital-tests.sql
 END
-echo success
+&& echo success
