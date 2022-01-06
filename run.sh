5c2bb3d80197575f408d38bceae3bb8ed88674ba
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 16:07:05 +0100

    changed the inside content of the command

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 git pull && docker-compose down 
 docker-compose up -d && cat < docker exec -t index-performance_postgres_1 bash <<EOF
-    su postgres
-    psql < /home/postgres/inital-tests.sql
+    su postgres \
+    && psql < /home/postgres/inital-tests.sql
 EOF \
-&& echo success
+&& echo "success"
