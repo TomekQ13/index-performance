1748fae9be085c512a5fa25d692e9b9dc929c2ec
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 14:57:53 +0100

    changed flash

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec -T index-performance_postgres_1 bash <<END
+docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<END
     su postgres
     psql < /home/postgres/inital-tests.sql
 END
