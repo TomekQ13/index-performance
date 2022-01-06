233958106c7fb948751f5715eb3a6e70a17c3462
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 16:39:29 +0100

    added EOF

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ git pull && docker-compose down
 docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<'EOF'
     echo connected \
     && su postgres \
-    && psql < /home/postgres/inital-tests.sql \
+    && psql < /home/postgres/inital-tests.sql
+EOF
