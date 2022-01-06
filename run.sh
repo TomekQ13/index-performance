01940dc7f739031214f32662bcd08db814a44d9f
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 16:38:57 +0100

    removed whitespace

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec -t index-performance_postgres_1 bash << 'EOF'
+docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<'EOF'
     echo connected \
     && su postgres \
     && psql < /home/postgres/inital-tests.sql \
