8fcdd587ca4242371856bf1ff2d80f30b2d3a13c
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 14:39:01 +0100

    prepared first testing script

--- initial-tests.sql
+++ initial-tests.sql
@@ -1,4 +1,4 @@
-drop table test;
+drop table if exists test;
 create table test (
 	id integer,
 	col_char_1 varchar(32),
@@ -11,12 +11,12 @@ create table test (
 
 );
 
-drop table test2;
+drop table if exists test2;
 create table test2 as
 select * from test
 where 1=0;
 
-drop sequence test_id_seq;
+drop sequence if exists test_id_seq;
 create sequence test_id_seq;
 
 CREATE OR REPLACE PROCEDURE public.insert_test_row()
@@ -41,50 +41,50 @@ end; $$
 
 insert into test2 select * from test;
 -- Query returned successfully in 5 secs 396 msec.
-truncate table test2;
 
+truncate table test2;
 create index id_idx on test2 (id);
 insert into test2 select * from test;
 
 
-do $$
-begin
+-- do $$
+-- begin
 
-	for loop_counter in 1..1000000 loop
-		call public.insert_test_row();
-	end loop;
+-- 	for loop_counter in 1..1000000 loop
+-- 		call public.insert_test_row();
+-- 	end loop;
 
-end; $$
--- 1 min 16 s
--- 1 min 18 s
+-- end; $$
+-- -- 1 min 16 s
+-- -- 1 min 18 s
 
-truncate table test;
-create index id_idx_2 on test (id, col_char_3, col_date);
+-- truncate table test;
+-- create index id_idx_2 on test (id, col_char_3, col_date);
 
-do $$
-begin
+-- do $$
+-- begin
 
-	for loop_counter in 1..1000000 loop
-		call public.insert_test_row();
-	end loop;
+-- 	for loop_counter in 1..1000000 loop
+-- 		call public.insert_test_row();
+-- 	end loop;
 
-end; $$
--- 1 min 22 s
--- 1 min 20 s
+-- end; $$
+-- -- 1 min 22 s
+-- -- 1 min 20 s
 
-truncate table test;
-create index id_idx_3 on test (col_char_2);
-create index id_idx_4 on test (col_char_3);
-create index id_idx_5 on test (col_num_1);
-create index id_idx_6 on test (col_num_2);
+-- truncate table test;
+-- create index id_idx_3 on test (col_char_2);
+-- create index id_idx_4 on test (col_char_3);
+-- create index id_idx_5 on test (col_num_1);
+-- create index id_idx_6 on test (col_num_2);
 
-do $$
-begin
+-- do $$
+-- begin
 
-	for loop_counter in 1..1000000 loop
-		call public.insert_test_row();
-	end loop;
+-- 	for loop_counter in 1..1000000 loop
+-- 		call public.insert_test_row();
+-- 	end loop;
 
-end; $$
--- 1 min 27 s
--- 1 min 25 s
+-- end; $$
+-- -- 1 min 27 s
+-- -- 1 min 25 s
