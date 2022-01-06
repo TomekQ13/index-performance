b520c7b63a75a1e5ab1bc3409176ba39b13f40d3
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 15:01:16 +0100

    end to eof

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git pull && docker-compose down 
-docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<END
+docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<EOF
     su postgres
     psql < /home/postgres/inital-tests.sql
-END \
+EOF \
 && echo success
