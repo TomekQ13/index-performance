c57113728aea6eaf264a41f33751bed2adc16fc3
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-06 16:24:29 +0100

    removed echo

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ git pull && docker-compose down
 docker-compose up -d && docker exec -t index-performance_postgres_1 bash <<EOF
     su postgres \
     && psql < /home/postgres/inital-tests.sql
-EOF \
-&& echo "success"
+EOF
