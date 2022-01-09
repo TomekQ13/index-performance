6670831c9f8c38ef66e7b4448737274e5e0067e0
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-09 23:35:45 +0100

    added comments

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ echo "Running inserts without any index..." \
 && for i in {1..10} 
 do
     echo "Running iteration $i"
+    # times the insert, redirects the stderr output to stream 1, removes unnecessary lines and removes newline character between iteration n and time and saves to a file
     { echo "Iteration $i " ; { time docker exec index-performance_postgres_1 bash /home/postgres/runInsert.sh ; } 2>&1 | sed -n 3p ; } | tr "\n" " " >> times.txt
+    # append a newline at the end of the iterations
     echo "" >> times.txt
 done 
