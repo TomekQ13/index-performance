7ab2dedd07ea8d3316fe174ec3c83bcc7ad7d557
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-09 23:21:59 +0100

    added iteration in the same line

--- run.sh
+++ run.sh
@@ -11,6 +11,5 @@ echo "Running inserts without any index..." \
 && for i in {1..10} 
 do
     echo "Running iteration $i"
-    echo "Iteration $i" >> times.txt
-    { time docker exec index-performance_postgres_1 bash /home/postgres/runInsert.sh ; } 2>&1 | sed -n 3p >> times.txt
+    { echo "Iteration $i" ; { time docker exec index-performance_postgres_1 bash /home/postgres/runInsert.sh ; } 2>&1 | sed -n 3p ; } >> times.txt
 done 
