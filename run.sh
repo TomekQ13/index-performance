0640b7a8fbcbc1cd7799ae9b46e685acf63ffdeb
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-09 22:31:44 +0100

    append instead of replace

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ echo "Running inserts without any index..." \
 do
     echo "Running iteration $i"
     echo "Iteration $1" >> times.txt
-    { time docker exec index-performance_postgres_1 bash /home/postgres/runInsert.sh ; } 2> times.txt
+    { time docker exec index-performance_postgres_1 bash /home/postgres/runInsert.sh ; } 2>> times.txt
 done 
