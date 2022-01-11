22dfcb573b0c8dd63c60da9e11cdf4d0ac3c14f1
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-11 22:36:29 +0100

    changed condition in if

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ echo "Preparing configuration..." \
 # running the insert without an index
 echo "Running inserts without any index..." \
 && i=0 \
-&& while [ "$i" < "$iterationCount" ]
+&& while [ "$i" -le "$iterationCount" ]
 do
     # times the insert, redirects the stderr output to stream 1, removes unnecessary lines and removes newline character between iteration n and time and saves to a file
     { echo "No indexes - Iteration $i " ; { time docker exec index-performance_postgres_1 bash /home/postgres/runSQL.sh /home/postgres/sql/insert.sql ; } 2>&1 | sed -n 3p ; } | tr "\n" " " >> $timesLogFile
@@ -23,10 +23,11 @@ do
 done \
 && time docker exec index-performance_postgres_1 bash /home/postgres/runSQL.sh /home/postgres/sql/createIndexId.sql \
 &&i=0 \
-&& while [ "$i" < "$iterationCount" ]
+&& while [ "$i" -le "$iterationCount" ]
 do
     # times the insert, redirects the stderr output to stream 1, removes unnecessary lines and removes newline character between iteration n and time and saves to a file
     { echo "ID index - Iteration $i " ; { time docker exec index-performance_postgres_1 bash /home/postgres/runSQL.sh /home/postgres/sql/insert.sql ; } 2>&1 | sed -n 3p ; } | tr "\n" " " >> $timesLogFile
     # append a newline at the end of the iterations
     echo "" >> $timesLogFile
+    i=$(($i + 1))
 done
