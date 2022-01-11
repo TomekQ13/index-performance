c329ade1285e15d61a6d438c23b27201934047d9
TomekQ13 <48492562+TomekQ13@users.noreply.github.com>
2022-01-11 23:21:30 +0100

    changed the insert script

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,13 @@
-insert into test2
-select *
-from test
-where id > (select max(id) from test2) and id <= (select max(id) from test2) + 10000;
+do $$
+declare
+    max_id integer;
+begin;
+
+    select coalesce(max(id),0) into max_id
+    from test2;
+
+    insert into test2
+    select *
+    from test
+    where id > max_id and id <= max_id + 10000;
+end; $$
